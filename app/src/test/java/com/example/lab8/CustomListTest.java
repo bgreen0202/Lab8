66ea4d18761fe2f638e50a95c0d7088b6260dfa0
bgreen0202 <bgreen@ualberta.ca>
2024-10-23 12:42:54 -0600

    Adding addCity functionality

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -24,8 +24,7 @@ public class CustomListTest {
     /**
      * get the size of the list
      * increase the list by adding a new city
-     * check if our current size matches the initial size
-     plus one
+     * check if our current size matches the initial size plus one
      */
     @Test
     public void addCityTest(){
@@ -34,5 +33,4 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
     }
-
 }
